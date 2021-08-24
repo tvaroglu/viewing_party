d2d4e51162a7fdf59b6d73fefbbc7cbe64774437
danembb <dbrophy720@gmail.com>
2021-08-24 16:50:04 -0600

    Complete user story 15

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-
+  root 'welcome#index'
+  resources :users, only: [:new, :create]
+  get "/login", to: "users#login"
+  post "/login", to: "users#authenticate"
 end
---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'welcome index page' do
+  before :each do
+    visit root_path
+  end
+  # Welcome Page
+  # When a user visits the root path they should be on the welcome page which includes:
+    # Welcome message X
+    # Brief description of the application X
+    # Button to Log in X
+    # Link to Registration X
+  it 'can display information about the application' do
+    expect(page).to have_content("Welcome to Viewing Party!")
+    expect(page).to have_content("Viewing Party is an application in which users can explore movie options and create a viewing party event for the user and their invited friends")
+  end
+
+  it 'can display a button to log in and a link to registration' do
+    expect(page).to have_link("Log In")
+    expect(page).to have_link("Register")
+  end
+
+  it 'links the users to log-in and register' do
+    click_on "Log In"
+    expect(current_path).to eq(login_path)
+
+    visit root_path
+    click_on "Register"
+    expect(current_path).to eq(new_user_path)
+  end
+end
