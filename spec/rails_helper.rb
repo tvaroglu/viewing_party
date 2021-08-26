6da81f930b65044f431653498b8199160c97fae9
Taylor Varoglu <tava8398@colorado.edu>
2021-08-26 13:12:02 -0600

    Add webmock stub to MovieService spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,7 @@ SimpleCov.start do
   add_filter 'app/channels/'
   add_filter 'app/jobs/'
   add_filter 'app/mailers/'
+  add_filter 'spec/'
 end
 
 
--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe MovieService do
   before :each do
     if Rails.application.credentials.movie_db.nil?
-      key = ''
-      allow_any_instance_of(Services::RequestEndpoints).to receive(:key).and_return(key)
+      @api_key = ''
+    else
+      @api_key = Rails.application.credentials.movie_db.values.last
     end
+    allow_any_instance_of(Services::RequestEndpoints).to receive(:key).and_return(@api_key)
   end
 
   it 'exists' do
@@ -46,16 +48,18 @@ RSpec.describe MovieService do
   end
 
   describe 'helper methods' do
-    xit 'can return the top 40 most popular movies' do
+    it 'can return the top 40 most popular movies' do
       # 1.) Need to stub the 'render_request' method with a mock response to keep test light-weight
+      mock_response = "{\"login\":\"tvaroglu\",\"id\":12345678,\"url\":\"https://api.github.com/users/tvaroglu\"}"
+      webmock_request = stub_request(:get, "https://api.themoviedb.org/3/discover/movie?api_key=#{@api_key}&sort_by=popularity.desc&page=1").
+        to_return(status: 200, body: mock_response)
       # 2.) Need to add expectations to test the helper method (TBD) that aggregates the top 40 movies
         # Awaiting additional feedback via Slack if anyone found a way to call 40 movies in one request..
-      page_1_endpoint = MovieService.endpoints[:most_popular]['1-20']
-      page_2_endpoint = MovieService.endpoints[:most_popular]['21-40']
+      allow(MovieService).to receive(:make_request).and_return(webmock_request.response.body)
 
-      page_1_response = MovieService.render_request(page_1_endpoint)['results']
-      page_2_response = MovieService.render_request(page_2_endpoint)['results']
-      require "pry"; binding.pry
+      page_1_endpoint = MovieService.endpoints[:most_popular]['1-20']
+      page_1_response = MovieService.render_request(page_1_endpoint)
+      expect(page_1_response['login']).to eq('tvaroglu')
     end
 
     xit 'can return up to 40 movies matching a passed in search criteria' do
