995b40f04551d7c51219c58501899e371e262163
Taylor Varoglu <tava8398@colorado.edu>
2021-08-26 22:33:35 -0600

    Add SessionsController to controllers and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  def login; end
-
   def new
     @user = User.new
   end
@@ -10,19 +8,17 @@ class UsersController < ApplicationController
     user[:email] = user[:email].downcase
     new_user = User.create(user)
     if new_user.save
-      redirect_to dashboard_path(new_user.id)
-      flash[:alert] = "Registration complete! Welcome, #{new_user.email}!"
+      redirect_to root_path
+      flash[:alert] = "New account successfully created for: #{new_user.email}!"
     else
       redirect_to registration_path
-      flash[:alert] = "Oops, couldn't create your account. Please make sure you are using a valid email and password."
+      flash[:alert] = "Invalid credentials, please try again."
     end
   end
 
   def show
     @user = User.find(params[:id])
   end
-
-  def authenticate; end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :users, only: [:create]
-  get '/users/:id', to: 'users#show', as: '/dashboard'
   get '/users', to: 'users#new', as: '/registration'
+  get '/users/:id', to: 'users#show', as: '/dashboard'
 
-  get '/login', to: 'users#login'
-  post '/login', to: 'users#authenticate'
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  get '/logout', to: "sessions#destroy"
+
+  # get '/login', to: 'users#login'
+  # post '/login', to: 'users#authenticate'
 end
