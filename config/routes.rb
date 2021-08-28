f9507d0dfed5661ddc5c6a33536a8114dee5a4ce
Taylor Varoglu <tava8398@colorado.edu>
2021-08-28 12:45:05 -0600

    Add placeholder route to /discover page from /dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'users#dashboard'
   post '/dashboard', to: 'users#search'
 
+  # placeholder, add route once page built
+  get '/discover', to: 'welcome#index'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
