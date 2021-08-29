704397f3502b64707974485e20e042a9a1058667
Taylor Varoglu <tava8398@colorado.edu>
2021-08-28 18:46:17 -0600

    Add helper method to Attendee

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,4 +1,8 @@
 class Attendee < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  def user_email
+    user.email
+  end
 end
--- spec/models/attendee_spec.rb
+++ spec/models/attendee_spec.rb
@@ -5,4 +5,26 @@ RSpec.describe Attendee do
     it { should belong_to(:user) }
     it { should belong_to(:event) }
   end
+
+  describe 'methods' do
+    before :each do
+      @taylor = User.create!(email: 'foo@bar.com', password: 'test')
+      @dane = User.create!(email: 'boo@far.com', password: 'nico')
+      @admin = User.create!(email: 'admin@example.com', password: 'guest')
+
+      @event = Event.create!(
+        user: @taylor,
+        movie_title: 'Kangaroo Jack',
+        event_date: Date.today,
+        event_time: Time.now,
+        runtime: 90
+      )
+    end
+
+    it "can return the user's email" do
+      attendee = Attendee.create!(user: @taylor, event: @event)
+
+      expect(@event.attendees.first.user_email).to eq(@taylor.email)
+    end
+  end
 end
