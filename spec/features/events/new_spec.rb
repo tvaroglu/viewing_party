787a523612b9271ed2ee3af25c1a6abafa819835
danembb <dbrophy720@gmail.com>
2021-08-31 17:13:27 -0600

    Update testing for new events

---
+++ spec/features/events/new_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+RSpec.describe 'event index page' do
+  before :each do
+    @event1 = Event.create!(movie_title: 'Suspiria', event_date: '2021-08-30'.to_date, event_time: '2021-08-30 08:00:00 -0600'.to_time, runtime: 125, user: @admin)
+    @taylor = User.create!(email: 'foo@bar.com', password: 'test')
+    @dane = User.create!(email: 'boo@far.com', password: 'nico')
+    @admin.followers << [@dane, @taylor]
+    visit new_event_path
+  end
+  # As an authenticated user,
+  # When I visit the new viewing party page,
+  # I should see the name of the movie title rendered above a form with the following fields: t
+  #   Duration of Party with a default value of movie runtime in minutes; a viewing party should NOT be created if set to a value less than the duration of the movie
+  #   When: field to select date X
+  #   Start Time: field to select time t
+  #   Checkboxes next to each friend (if user has friends) t
+  #   Button to create a party t
+  # Whenever a user creates an event, should designate the creator as an attendee
+  # @attendee_1 = Attendee.create!(user: @taylor, event: @event_1)
+  # @attendee_2 = Attendee.create!(user: @admin, event: @event_1)
+  it 'can display the movie title with all form information' do
+    expect(page).to have_content('Create New Viewing Party')
+    expect(page).to have_content('Event Date:')
+    expect(page).to have_content('Start Time:')
+    expect(page).to have_content('Your Current Followers:')
+    expect(page).to have_button('Create Event')
+  end
+
+  #Happy Path
+  it 'can enter in event information and create a new event' do
+    fill_in :movie_title, with: @event1.movie_title
+    fill_in :event_date, with: '2021/08/30'
+    fill_in :runtime, with: @event1.runtime
+    select '08 AM', :from => "event[event_time(4i)]"
+    select '00', :from => "event[event_time(5i)]"
+
+    within "#follower-#{@admin.followers.first.id}" do
+      check 'invited[]'
+    end
+    within "#follower-#{@admin.followers.last.id}" do
+      check 'invited[]'
+    end
+
+    click_on 'Create Event'
+
+    expect(current_path).to eq(dashboard_path(@admin.id))
+    expect(page).to have_content("Host: #{@admin.email}")
+    expect(page).to have_content("Date: #{ApplicationRecord.format_date(@event1.event_date)}")
+    expect(page).to have_content("Time: #{ApplicationRecord.format_time(@event1.event_time)}")
+
+    # Add div-id block for within block
+    expect(page).to have_content("1.) #{@taylor.email}")
+    expect(page).to have_content("2.) #{@dane.email}")
+    expect(page).to have_content("3.) #{@admin.email}")
+  end
+
+  #Sad Path
+  it 'can redirect back to new page if improper/no information provided and display error message' do
+    fill_in :movie_title, with: @event1.movie_title
+    fill_in :event_date, with: '2021/08/30'
+    fill_in :runtime, with: @event1.runtime
+    click_on 'Create Event'
+
+    expect(current_path).to eq(new_event_path)
+    expect(page).to have_content('Error: You must invite followers to your party,')
+    expect(page).to have_field(:movie_title, with: @event1.movie_title)
+    expect(page).to have_field(:runtime, with: @event1.runtime)
+  end
+end
