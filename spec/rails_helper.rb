144befcea79885859e206560d89a1fc2e7c1dfb5
Taylor Varoglu <tava8398@colorado.edu>
2021-09-02 18:49:42 -0600

    Add unnecessary hook in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -51,12 +51,6 @@ RSpec.configure do |config|
     @admin = User.create!(email: 'admin@example.com', password: 'guest')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
 
-    # # hook for encrypted credentials (for TravisCI builds to pass)
-    # if Rails.application.credentials.movie_db.nil?
-    #   @api_key = ''
-    #   allow_any_instance_of(Services::RequestEndpoints).to receive(:key).and_return(@api_key)
-    # end
-
     # hook for GET /config endpoint:
     @config_blob = File.read('./spec/fixtures/config.json')
     @config_request = stub_request(:get, "https://api.themoviedb.org/3/configuration?api_key=#{@api_key}").
